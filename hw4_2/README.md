57547e6610c8ef2385703715ff54d5153bac50a7
jialicatherine <43858610+jialicatherine@users.noreply.github.com>
2020-04-21 02:44:40 +0000

    Add files via upload

---
+++ hw4_2/README.md
@@ -0,0 +1,61 @@
+# HW 4 Let's Network
+## Overview
+
++ `cache_server.cc`: This file inludes a main function which calls whatever functions it needs to set up a cache, and establish a receiving (TCP) socket. It then loops indefinitely, listening for messages, processing the requests, and returning the appropriate responses.
++ `cache.hh`: Includes a new constructor which establishes a connection with the server.
++ `cache_client.cc`: Cache client object whih holds connection information to access the cache over the network.
++ `test_cache_client.cc`: Same test as hw3 but with cache client object.
+
+Other files stay the same as hw3.
+
+## Compile and Run
+
+### Server
+We use crow(github address: https://github.com/ipkn/crow) to construct the cache_server.cc.
+1. We followed its instruction to download boost.
+2. Due to the version of boost used in crow, we modified the "CSCI389-Computer-System-Partner-Work\crow\include\crow\include\crow\socket.adptors.hh" according to https://git.xmr.pm/wownero/onion-wownero-blockchain-explorer/commit/444effc2878ae714feb9c3087c81b4f2f25975be. (We have included the new crow folders and so there is no need for you to implement this step.)
+3. To run the server, use the command line:
+```
+g++ -std=c++17 -I../crow/include -Wall -Wextra cache_lib.cc cache_server.cc fifo_evictor.cc -o server -pthread -lboost_system
+./server 5 11112
+```
+With one terminal window running, we open another window and input commands like:
+```
+curl -X Put localhost:11112/keyval/`key`/`value`
+curl -I localhost:11112/head
+curl -X GET localhost:11112/key/`key`
+curl -X Post localhost:11112/reset
+curl -X DELETE localhost:11112/key/`key`
+```
+
+### Client
+
+We use libcurl (https://curl.haxx.se/libcurl/) to construct the cache_client.cc.
+1. Since there is no explicit way to retrieve data in libcurl, we write a callback function according to https://curl.haxx.se/mail/lib-2012-06/0308.html. Then we use this callback function, CURLOPT_WRITEFUNCTION and CURLOPT_WRITEDATA in libcurl to implement the retrieval.
+2. To run the client and test it, we keep the server running (with ./server 5 11112), and then use the command line:
+```
+client: g++ -std=c++17 -o client cache_client.cc test_cache_client.cc  -lcurl
+./client
+```
+
+## Test
+| Test Name | Desciption | Test Status |
+| :--- |:---: | :---: |
+| Set/Get 1 | `set` a key and check if the value of this existed key is correctly returned by calling `get`. | Pass |
+| Set/Get 2 | `set` a key and check if the size of the existed key's value is correctly returned by calling `get`. | Pass |
+| Set/Get 3 | Check if the value of a nonexisted key is correctly returned by calling `get`.  | Pass |
+| Set/Get 4 | Check if the value of the size of a nonexisted key's value is correctly returned by calling `get`.  | Pass |
+| Del 1 | Check if the selected key is successfully deleted from the cache by calling `del` and the used_space has decreases. | Pass |
+| Del 2 | Check that the selected key cannot be got after the deletion. | Fail |
+| Del 3 | Check that a nonexisted key cannot be deleted. | Fail |
+| Space_used1 | Check if the initial Cache has 0 space used. | Pass |
+| Space_used2 | Check if Cache has correct space used after setting keys. | Pass |
+| Space_used3 | Check if Cache has correct space used after deleting keys. | Fail |
+| Evict 1 | Check if Cache evicts enough space for newly added key when only one key need to be evicted, and the eviction follows the right order. |  Pass |
+| Evict 2 | Check if Cache evicts enough space for a newly added key when multiple keys need to be evicted, and the eviction follows the right order. |  Pass |
+| Reset 1 | Check if `reset` cleans Cache and gives back 0 used space | Pass |
+| Reset 2 | Check that, after `reset` cleans Cache, no key can be retrieved | Pass |
+
+Reason:
+We have those failures every time we directly call del/key/<string> (from cache_server.cc) for nonexisted key.
+It gave us "Bus error 10" and "Segmentation fault", so we thought the failures are caused by some memory allocation problems existing in DELETE method in cache_server.cc. This can also be seen when we run `valgrind` on `./server` and `./client`, which showed some exisiting memory leaks.
---
+++ hw4_2/testy_cache.cc
@@ -0,0 +1,332 @@
+#define CATCH_CONFIG_MAIN
+#include "catch.hpp"
+
+#include <iostream>
+#include <cstdlib>
+#include <cassert>
+#include <cstring>
+
+#include "cache.hh"
+#include "fifo_evictor.hh"
+
+using namespace std;
+
+// Initialize cache and key/value variables/pointers
+FIFO_Evictor* e = new FIFO_Evictor();
+Cache c(10,0.75,e,std::hash<key_type>());
+// For testing null evictor.
+FIFO_Evictor* e1 = nullptr;
+Cache c1(10,0.75,e1,std::hash<key_type>());
+
+TEST_CASE("Set/Get")
+{
+    SECTION("The value of the existed key is returned correctly.")
+    {
+        c.reset();
+        key_type key = "a";
+      	Cache::val_type val = "z";
+      	Cache::size_type size = strlen(val)+1;
+      	c.set(key, val, size);
+      	key_type key2 = "b";
+      	Cache::val_type val2 = "xy";
+      	Cache::size_type size2 = strlen(val2)+1;
+      	c.set(key2, val2, size2);
+
+        Cache::val_type get_val = c.get(key, size2);
+        REQUIRE(strcmp(get_val,"z") == 0);
+        c.reset();
+    }
+
+    SECTION("The size of the existed key's value is returned correctly.")
+    {
+        c.reset();
+        key_type key = "a";
+        Cache::val_type val = "z";
+        Cache::size_type size = strlen(val)+1;
+        c.set(key, val, size);
+        key_type key2 = "b";
+        Cache::val_type val2 = "xy";
+        Cache::size_type size2 = strlen(val2)+1;
+        c.set(key2, val2, size2);
+
+        Cache::val_type get_val = c.get(key, size2);
+        REQUIRE(size2 == size);
+        c.reset();
+    }
+
+    SECTION("The value of the nonexisted key is returned correctly.")
+    {
+        c.reset();
+        key_type key = "a";
+        Cache::val_type val = "z";
+        Cache::size_type size = strlen(val)+1;
+        c.set(key, val, size);
+        key_type key2 = "b";
+        Cache::val_type val2 = "xy";
+        Cache::size_type size2 = strlen(val2)+1;
+        c.set(key2, val2, size2);
+
+        key_type fake_key = "o";
+        auto get_fake_val = c.get(fake_key, size);
+        REQUIRE(get_fake_val == nullptr);
+        c.reset();
+    }
+
+    SECTION("The size of the nonexisted key's value is returned correctly.")
+    {
+        c.reset();
+        key_type key = "a";
+        Cache::val_type val = "z";
+        Cache::size_type size = strlen(val)+1;
+        c.set(key, val, size);
+        key_type key2 = "b";
+        Cache::val_type val2 = "xy";
+        Cache::size_type size2 = strlen(val2)+1;
+        c.set(key2, val2, size2);
+
+        key_type fake_key = "o";
+        auto get_fake_val = c.get(fake_key, size);
+        REQUIRE(size == 0);
+        c.reset();
+    }
+}
+
+TEST_CASE("Del")
+{
+    SECTION("The selected key is deleted from the cache successfully.")
+    {
+        c.reset();
+        key_type key = "a";
+        Cache::val_type val = "z";
+        Cache::size_type size = strlen(val)+1;
+        c.set(key, val, size);
+
+        bool del_flag = c.del(key);
+        REQUIRE(del_flag == true);
+        REQUIRE(c.space_used() == 0);
+        c.reset();
+    }
+
+    SECTION("The selected key cannot be got after the deletion.")
+    {
+        c.reset();
+        key_type key = "a";
+        Cache::val_type val = "z";
+        Cache::size_type size = strlen(val)+1;
+        c.set(key, val, size);
+
+        bool del_flag = c.del(key);
+        auto get_deleted_val = c.get(key, size);
+        REQUIRE(get_deleted_val == nullptr);
+        REQUIRE(size == 0);
+        c.reset();
+    }
+
+    SECTION("The value of the nonexisted key cannot be deleted.")
+    {
+        c.reset();
+        key_type key = "a";
+        Cache::val_type val = "z";
+        Cache::size_type size = strlen(val)+1;
+        c.set(key, val, size);
+
+        key_type key2 = "b";
+        bool del_flag = c.del(key2);
+        REQUIRE(del_flag == false);
+        REQUIRE(c.space_used() == size);
+        c.reset();
+    }
+
+}
+
+TEST_CASE("Space_used")
+{
+    SECTION("Ensure that initial Cache has 0 space used.")
+    {
+        c.reset();
+        REQUIRE(c.space_used() == 0);
+        c.reset();
+    }
+
+    SECTION("Ensure that Cache has coreect space used after setting pairs.")
+    {
+        c.reset();
+        key_type key = "a";
+        Cache::val_type val = "z";
+        Cache::size_type size = strlen(val)+1;
+        c.set(key, val, size);
+
+        key_type key2 = "b";
+        Cache::val_type val2 = "xy";
+        Cache::size_type size2 = strlen(val2)+1;
+        c.set(key2, val2, size2);
+
+        REQUIRE(c.space_used() == size+size2);
+        c.reset();
+    }
+
+    SECTION("Ensure that Cache has coreect space used after deleting pairs.")
+    {
+        c.reset();
+        key_type key = "a";
+        Cache::val_type val = "z";
+        Cache::size_type size = strlen(val)+1;
+        c.set(key, val, size);
+
+        key_type key2 = "b";
+        Cache::val_type val2 = "xy";
+        Cache::size_type size2 = strlen(val2)+1;
+        c.set(key2, val2, size2);
+
+        c.del(key2);
+        REQUIRE(c.space_used() == size);
+
+        c.del(key);
+        REQUIRE(c.space_used() == 0);
+        c.reset();
+    }
+
+}
+
+
+TEST_CASE("Evict")
+{
+    SECTION("Check when evictor is a nullptr")
+    {
+
+        c1.reset();
+        key_type key1 = "a";
+        key_type key2 = "b";
+        key_type key3 = "c";
+        key_type key4 = "d";
+        Cache::val_type val = "z";
+        Cache::size_type size = strlen(val)+1;
+        c1.set(key1, val, size);
+        c1.set(key2, val, size);
+        c1.set(key3, val, size);
+        c1.set(key4, val, size);
+
+        key_type key6 = "f";
+        Cache::val_type val6 = "yz";
+        Cache::size_type size6 = strlen(val6)+1;
+        c1.set(key6, val6, size6);
+        REQUIRE(c1.space_used() == 8);
+        auto cannotadd = c1.get(key6, size6);
+        REQUIRE(cannotadd == nullptr);
+        REQUIRE(size6 == 0);
+        c1.reset();
+    }
+
+    SECTION("Ensure that when only one key gets evicted, Cache has correct used space and the order of the eviction is correct.")
+    {
+        c.reset();
+        key_type key1 = "a";
+        key_type key2 = "b";
+        key_type key3 = "c";
+        key_type key4 = "d";
+        key_type key5 = "e";
+        Cache::val_type val = "z";
+        Cache::size_type size = strlen(val)+1;
+        c.set(key1, val, size);
+        c.set(key2, val, size);
+        c.set(key3, val, size);
+        c.set(key4, val, size);
+        c.set(key5, val, size);
+
+        key_type key6 = "f";
+        c.set(key6, val, size);
+        REQUIRE(c.space_used() == 10);
+        auto evict_val = c.get(key1, size);
+        REQUIRE(evict_val == nullptr);
+        REQUIRE(size == 0);
+        c.reset();
+    }
+
+    SECTION("Ensure that Cache evicts enough space for newly added key when multiple keys need to be evicted.")
+    {
+        c.reset();
+        key_type key1 = "a";
+        key_type key2 = "b";
+        key_type key3 = "c";
+        key_type key4 = "d";
+        key_type key5 = "e";
+        Cache::val_type val = "z";
+        Cache::size_type size = strlen(val)+1;
+        c.set(key1, val, size);
+        c.set(key2, val, size);
+        c.set(key3, val, size);
+        c.set(key4, val, size);
+        c.set(key5, val, size);
+
+        key_type key7 = "g";
+      	Cache::val_type val7 = "yz";
+      	Cache::size_type size7 = strlen(val7)+1;
+      	c.set(key7, val7, size7);
+      	REQUIRE(c.space_used() == 9);
+
+      	auto evict_val1 = c.get(key1, size7);
+      	REQUIRE(evict_val1 == nullptr);
+      	REQUIRE(size7 == 0);
+      	size7 = 10; //redefine size7 to check
+      	auto evict_val2 = c.get(key2, size7);
+      	REQUIRE(evict_val2 == nullptr);
+      	REQUIRE(size7 == 0);
+        c.reset();
+    }
+
+}
+
+
+TEST_CASE("Reset")
+{
+
+    SECTION("Ensure that reset cleans the Cache and gives back 0 used space.")
+    {
+        c.reset();
+        key_type key1 = "a";
+      	key_type key2 = "b";
+      	key_type key3 = "c";
+      	key_type key4 = "d";
+      	key_type key5 = "e";
+      	Cache::val_type val = "z";
+      	Cache::size_type size = strlen(val)+1;
+      	c.set(key1, val, size);
+      	c.set(key2, val, size);
+      	c.set(key3, val, size);
+      	c.set(key4, val, size);
+      	c.set(key5, val, size);
+
+      	REQUIRE(c.space_used() != 0);
+
+      	c.reset();
+      	REQUIRE(c.space_used() == 0);
+
+        c.reset();
+    }
+
+    SECTION("Ensure that after reset cleans the Cache and no key can be retrieved.")
+    {
+        c.reset();
+        key_type key1 = "a";
+        key_type key2 = "b";
+        key_type key3 = "c";
+        key_type key4 = "d";
+        key_type key5 = "e";
+        Cache::val_type val = "z";
+        Cache::size_type size = strlen(val)+1;
+        c.set(key1, val, size);
+        c.set(key2, val, size);
+        c.set(key3, val, size);
+        c.set(key4, val, size);
+        c.set(key5, val, size);
+
+        c.reset();
+        auto get_val = c.get(key1, size);
+        REQUIRE(get_val == nullptr);
+        REQUIRE(size == 0);
+
+        c.reset();
+
+    }
+
+}
